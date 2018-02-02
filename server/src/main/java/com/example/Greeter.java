ba2b0d029d96e950e19b8106c38bd18120162c8b
Keerati Limkulphong <imkrish@Keeratis-MBP.hub>
2018-02-03 11:17:40 +1300

    fix checkstyles warning

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,8 +12,10 @@ public class Greeter {
 
   }
 
-  //TODO: Add javadoc comment
-  public String greet(String someone) {
+  /**
+   * Greet people with someone name
+   */
+  public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
--- server/src/test/java/com/example/TestGreeter.java
+++ server/src/test/java/com/example/TestGreeter.java
@@ -19,7 +19,7 @@ public class TestGreeter {
 
   @Test
   public void greetShouldIncludeTheOneBeingGreeted() {
-    String someone = "Worlds";
+    String someone = "World";
 
     assertThat(greeter.greet(someone), containsString(someone));
   }
